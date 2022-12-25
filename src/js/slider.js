f71ebd962310e842de130267cd259fee49760136
lucashtml6 <lucassouzarhcp9@gmail.com>
2022-12-24 21:28:33 -0300

    feat: adicionando aba Projetos
    - Separei os códigos CSS por páginas
    - Adicionei todos os links no menu

---
+++ src/js/slider.js
@@ -0,0 +1,52 @@
+const setas = document.getElementsByClassName("seta");
+const setaDireita = document.getElementById("seta-direita")
+const setaEsquerda = document.getElementById("seta-esquerda")
+const cards = document.getElementsByClassName("container-projeto")
+
+console.log(setas);
+
+let indiceImagem = 0;
+
+
+setaDireita.addEventListener('click', () => {
+  indiceImagem++
+  setaEsquerda.disabled = false
+
+  cards[indiceImagem-1].classList.remove("mostrar")
+  cards[indiceImagem].classList.add("mostrar")
+
+  if(indiceImagem > 0) {
+    setas[0].classList.remove("opacidade")
+  }
+
+  if(indiceImagem === 5) {
+    setas[1].classList.add("opacidade")
+    setaDireita.disabled = true
+  }    
+})
+
+setaEsquerda.addEventListener('click', () => {
+  indiceImagem--
+  setaDireita.disabled = false
+
+  if(indiceImagem < 0) {
+    indiceImagem = 0
+  } else if (indiceImagem >5) {
+    indiceImagem = 5
+  } 
+
+  console.log(indiceImagem)
+  cards[indiceImagem+1].classList.remove("mostrar")
+  cards[indiceImagem].classList.add("mostrar")
+
+  if(indiceImagem < 5) {
+    setas[1].classList.remove("opacidade")
+  }
+
+  if(indiceImagem === 0) {
+    setas[0].classList.add("opacidade")
+    setaEsquerda.disabled = true
+  } else {
+    setaEsquerda.disabled = false
+  }
+})
